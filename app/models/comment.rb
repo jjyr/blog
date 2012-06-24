c23d5883d7c69fc3a3ad8b0c1de7e70e45fbf5ec
jjy <jjyruby@gmail.com>
2012-06-24 22:49:48 +0800

    refactory validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,12 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :email, :ip, :name
 
   validates :name,presence: true
-  validates :name,length: 1..10,unless: ->{name.blank?}
+  validates :name,length: 1..10,allow_blank: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true
-  validates :email,format: {with: VALID_EMAIL_REGEX},unless: ->{email.blank?}
+  validates :email,format: {with: VALID_EMAIL_REGEX},allow_blank: true
   validates :body,presence: true
-  validates :body,length: 1..200,unless: ->{body.blank?}
+  validates :body,length: 1..200,allow_blank: true
 
   default_scope order("created_at")
 end
