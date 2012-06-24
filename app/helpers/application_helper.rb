12df321a8505266e87625857f54c17fe7a989988
jjy <jjyruby@gmail.com>
2012-06-24 19:35:05 +0800

    fixed timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+
+  TIME_ZONE = "Beijing"
+
   include PostsHelper
 
   def full_title title = nil
@@ -14,6 +17,6 @@ module ApplicationHelper
   end
 
   def format_date date
-    date.localtime.strftime "%Y-%m-%d %H:%M:%S"
+    date.localtime.in_time_zone(ApplicationHelper::TIME_ZONE).strftime "%Y-%m-%d %H:%M:%S"
   end
 end
