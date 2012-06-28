ba153684f8a09508f64c6c7420fb73bbfabaa0a7
jjy <jjyruby@gmail.com>
2012-06-28 21:35:14 +0800

    refactor posts_helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,18 +11,13 @@ module PostsHelper
     tags.split TAGS_REGEX
   end
 
-  def add_tags post,tags
-    split_tags(tags).each do |tag|
-      post.tags.create name: tag
-    end
-  end
 
   def update_tags post,tags
     tags = split_tags(tags).map do |tag|
-      post.tags.build name: tag
+      post.tags.create name: tag
     end
-    old_tags = @post.tags.all
-    (old_tags - tags).each &:destroy
-    (tags - old_tags).each &:save
+    @post.tags = tags
   end
+
+  alias add_tags update_tags
 end
