04ea76ddb8780b74ab1656c063ce58ac280c4425
jjy <jjyruby@gmail.com>
2012-07-01 01:43:35 +0800

    fix js error

--- app/assets/javascripts/blog_comments.js
+++ app/assets/javascripts/blog_comments.js
@@ -8,10 +8,10 @@ function ref_comment(number){
   var comment = $('#cbody' + number);
   var text = "\r" + ">[引用自#" + number + " " + $('#cname' + number).attr('cname') + "](#c" + number + ")\r>\r";
   text += trim(comment.text()).substr(0,ref_limit).replace(/\n/g,"\r>");
-  if(comment.text().length > ref_limit){
+  if(comment.val().length > ref_limit){
     text += "...";
   }
   text += "\r\r***";
-  $('#comment_form_body').text($('#comment_form_body').text() + text + "\r\r");
+  $('#comment_form_body').val($('#comment_form_body').val() + text + "\r\r");
   $('#comment_form_body').focus();
 }
